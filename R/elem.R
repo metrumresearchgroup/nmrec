35c810759d43b9d267f601332665dbc5830335e4
Kyle Meyer <kylem@metrumrg.com>
2023-03-20 10:35:36 -0400

    add helper for splitting record lines
    The plan is to use this as the initial step of within-record parsing.

---
+++ R/elem.R
@@ -0,0 +1,220 @@
+#' Split up a records lines into sub-parts
+#'
+#' The goal here is to split at every component that may be relevant for
+#' downstream parsing, letting the downstream decide whether some elements
+#' should be combined again (e.g., elements that are actually in a trailing
+#' comment).
+#'
+#' @param lines Character vector of lines for a record.
+#' @return A list of character scalars, with recognized "elements" marked by
+#'   `nmrec_element` subclass.
+#' @noRd
+split_to_elements <- function(lines) {
+  matches <- gregexec("[ \t]+|[,;=&()'\"]", lines)
+  ln_delim_idxs <- purrr::map(matches, as.vector)
+  ln_offsets <- purrr::map(
+    matches,
+    ~ as.vector(attr(.x, "match.length")) - 1
+  )
+
+  n_lines <- length(lines)
+  elems <- vector(
+    "list",
+    # Double number of delimiter matches to count the delimiter itself and the
+    # element (if any) to the left side.
+    2 * length(unlist(ln_delim_idxs)) +
+      # For each line, also count the non-delimiter element at the end of the
+      # string (if any) and an item for the newline.
+      2 * n_lines
+  )
+  elem_idx <- 1
+
+  # ATTN: Hold off on vectorizing or otherwise optimizing until implementation
+  # settles along with tests.
+  for (ln_idx in seq_along(lines)) {
+    ln <- lines[[ln_idx]]
+    ln_width <- nchar(ln)
+    if (!ln_width) {
+      elems[[elem_idx]] <- elem_linebreak()
+      elem_idx <- elem_idx + 1
+      next
+    }
+
+    if (startsWith(ln, ";")) {
+      elems[[elem_idx]] <- elem_comment(ln)
+      elems[[elem_idx + 1]] <- elem_linebreak()
+      elem_idx <- elem_idx + 2
+      next
+    }
+
+    delim_idxs <- ln_delim_idxs[[ln_idx]]
+    offsets <- ln_offsets[[ln_idx]]
+
+    if (identical(delim_idxs[1], -1L)) {
+      elems[[elem_idx]] <- ln
+      elems[[elem_idx + 1]] <- elem_linebreak()
+      elem_idx <- elem_idx + 2
+      next
+    }
+
+    # Scan along line, extracting one non-delimiter stretch and one delimiter
+    # stretch per iteration.
+    last_end <- 0
+    for (char_idx in seq_along(delim_idxs)) {
+      beg <- delim_idxs[char_idx]
+      end <- beg + offsets[char_idx]
+      if (identical(beg, 1L)) {
+        elems[[elem_idx]] <- elem_select(substr(ln, beg, end))
+        elem_idx <- elem_idx + 1
+        last_end <- end
+      } else {
+        if (beg - last_end > 1) {
+          elems[[elem_idx]] <- substr(ln, last_end + 1, beg - 1)
+          elem_idx <- elem_idx + 1
+        }
+        elems[[elem_idx]] <- elem_select(substr(ln, beg, end))
+        elem_idx <- elem_idx + 1
+        last_end <- end
+      }
+    }
+
+    # Extract remaining non-delimiter stretch.
+    if (last_end < ln_width) {
+      elems[[elem_idx]] <- substr(ln, last_end + 1, ln_width)
+      elem_idx <- elem_idx + 1
+    }
+
+    elems[[elem_idx]] <- elem_linebreak()
+    elem_idx <- elem_idx + 1
+  }
+
+  return(purrr::compact(elems))
+}
+
+elem_select <- function(x) {
+  switch(x,
+    "=" = elem_equal_sign(),
+    ";" = elem_semicolon(),
+    "&" = elem_ampersand(),
+    "," = elem_comma(),
+    "\"" = elem_quote_double(),
+    "'" = elem_quote_single(),
+    "(" = elem_paren_open(),
+    ")" = elem_paren_close(),
+    elem_whitespace(x)
+  )
+}
+
+elem_ampersand <- function() {
+  structure("&",
+    class = c(
+      "nmrec_ampersand",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_comment <- function(x) {
+  structure(x,
+    class = c(
+      "nmrec_comment",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_comma <- function() {
+  structure(",",
+    class = c(
+      "nmrec_comma",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_equal_sign <- function() {
+  structure("=",
+    class = c(
+      "nmrec_equal_sign",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_semicolon <- function() {
+  structure(";",
+    class = c(
+      "nmrec_semicolon",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_linebreak <- function() {
+  structure("\n",
+    class = c(
+      "nmrec_linebreak",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_paren_close <- function() {
+  structure(")",
+    class = c(
+      "nmrec_paren_close",
+      "nmrec_paren",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_paren_open <- function() {
+  structure("(",
+    class = c(
+      "nmrec_paren_open",
+      "nmrec_paren",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_quote_double <- function() {
+  structure("\"",
+    class = c(
+      "nmrec_quote_double",
+      "nmrec_quote",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_quote_single <- function() {
+  structure("'",
+    class = c(
+      "nmrec_quote_single",
+      "nmrec_quote",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
+
+elem_whitespace <- function(x) {
+  structure(x,
+    class = c(
+      "nmrec_whitespace",
+      "nmrec_element",
+      "character"
+    )
+  )
+}
---
+++ tests/testthat/test-elem.R
@@ -0,0 +1,88 @@
+test_that("split_to_elements() works", {
+  cases <- list(
+    list(
+      input = "",
+      want = list(elem_linebreak())
+    ),
+    list(
+      input = c("", "foo", ""),
+      want = list(elem_linebreak(), "foo", elem_linebreak(), elem_linebreak())
+    ),
+    list(
+      input = "    ",
+      want = list(elem_whitespace("    "), elem_linebreak())
+    ),
+    list(
+      input = "    ",
+      want = list(elem_whitespace("    "), elem_linebreak())
+    ),
+    list(
+      input = "foo",
+      want = list("foo", elem_linebreak())
+    ),
+    list(
+      input = c("foo", "bar"),
+      want = list("foo", elem_linebreak(), "bar", elem_linebreak())
+    ),
+    list(
+      input = c("", "  foo bar"),
+      want = list(
+        elem_linebreak(),
+        elem_whitespace("  "),
+        "foo", elem_whitespace(" "), "bar",
+        elem_linebreak()
+      )
+    ),
+    list(
+      input = c(" foo  bar", "baz\t "),
+      want = list(
+        elem_whitespace(" "), "foo", elem_whitespace("  "),
+        "bar", elem_linebreak(),
+        "baz", elem_whitespace("\t "), elem_linebreak()
+      )
+    ),
+    list(
+      input = c("; foo", "bar ; baz"),
+      want = list(
+        elem_comment("; foo"), elem_linebreak(),
+        "bar", elem_whitespace(" "),
+        elem_semicolon(), elem_whitespace(" "),
+        "baz", elem_linebreak()
+      )
+    ),
+    list(
+      input = c(" \t ; foo  ", "bar ; baz"),
+      want = list(
+        elem_whitespace(" \t "), elem_semicolon(),
+        elem_whitespace(" "), "foo", elem_whitespace("  "), elem_linebreak(),
+        "bar", elem_whitespace(" "),
+        elem_semicolon(), elem_whitespace(" "),
+        "baz", elem_linebreak()
+      )
+    ),
+    list(
+      input = c("fo,o\t&;", "bar=baz &", "\"'", "(abc)"),
+      want = list(
+        "fo", elem_comma(), "o", elem_whitespace("\t"),
+        elem_ampersand(), elem_semicolon(), elem_linebreak(),
+        "bar", elem_equal_sign(), "baz", elem_whitespace(" "),
+        elem_ampersand(), elem_linebreak(),
+        elem_quote_double(), elem_quote_single(), elem_linebreak(),
+        elem_paren_open(), "abc", elem_paren_close(), elem_linebreak()
+      )
+    )
+  )
+
+  for (case in cases) {
+    res <- split_to_elements(case$input)
+    expect_identical(!!res, !!case$want)
+    # Inputs and results match when rendered as string.
+    expect_identical(
+      paste0(paste0(unlist(!!res), collapse = "")),
+      paste0(
+        paste0(!!case$input, collapse = "\n"),
+        "\n"
+      )
+    )
+  }
+})
