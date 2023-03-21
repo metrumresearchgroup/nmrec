092f76746069c5542962fe60989207f6f05b2c87
Kyle Meyer <kylem@metrumrg.com>
2023-03-21 09:14:43 -0400

    fix deparse_string comment

--- R/util.R
+++ R/util.R
@@ -1,4 +1,4 @@
-# Reduced version of `deparse1()` isn't available until R 4.0.
+# Reduced version of `deparse1()`, which isn't available until R 4.0.
 deparse_string <- function(expr) {
   paste(deparse(expr, width.cutoff = 500L), collapse = " ")
 }
