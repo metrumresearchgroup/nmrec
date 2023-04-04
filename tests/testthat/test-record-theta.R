metrumresearchgroup/nmrec

commit dd51347d04ee2c3a967d88317d56f2a3a4ff87aa

rework template class to use a single list
record_parser and the parsing for parameter values use a template
object that consists of two lists, `template` and `values`.  `values`
contains the nmrec_option objects, and `template` includes all other
objects, with integers pointing to a positions in `values`.

This works, but keeping two lists is unnecessary and complicates both
the code and test cases.  Instead just store all of the values as is
in a single list.  In cases where a list restricted to the
nmrec_option objects is needed, it's easy enough to filter the list
down to those.

R/option.R
@@ -74,43 +74,16 @@ option_param <- R6::R6Class(
   "nmrec_option_param",
   inherit = option_pos,
   public = list(
-    template = NULL,
     values = NULL,
-    initialize = function(name, template, values) {
+    initialize = function(name, values) {
       super$initialize(name = name)
-      self$template <- template
       self$values <- values
     },
     format = function() {
-      if (is.null(self$template)) {
+      if (is.null(self$values)) {
         ""
       } else {
-        parts <- purrr::map(self$template, ~ {
-          if (inherits(.x, "nmrec_element")) {
-            value <- .x
-          } else if (identical(length(.x), 1L) && is.integer(.x)) {
-            opt <- self$values[[.x]]
-            if (is.null(opt)) {
-              abort(
-                sprintf("Template element %s not found", .x),
-                "nmrec_dev_error"
-              )
-            }
-            value <- opt$format()
-          } else {
-            abort(
-              c(
-                "Got unexpected value for template element.",
-                deparse_string(.x)
-              ),
-              "nmrec_dev_error"
-            )
-          }
-
-          return(value)
-        })
-
-        paste(parts, collapse = "")
+        lstr_format(self$values)
       }
     }
   )

R/record-data.R
@@ -26,7 +26,7 @@ parse_data_record <- function() {
       )
     }
 
-    rp$options_append(option_pos$new("filename", value = filename))
+    rp$append(option_pos$new("filename", value = filename))
   } else {
     # Parsing the previous $DATA record is necessary to decide how to parse any
     # subsequent ones.
@@ -36,11 +36,11 @@ parse_data_record <- function() {
   rp$gobble()
 
   file_only <- !is.null(filename) ||
-    identical(names(prev$options), "filename")
+    identical(purrr::map_chr(prev$get_options(), "name"), "filename")
   # (format)
   if (file_only && rp$elems_is("paren_open")) {
     pos <- find_closing_paren(rp)
-    rp$options_append(
+    rp$append(
       option_pos$new("format", value = rp$elems_yank_to(pos))
     )
   }
@@ -48,7 +48,7 @@ parse_data_record <- function() {
   rp$process_options()
   rp$elems_assert_done()
 
-  return(list(template = rp$get_template(), options = rp$get_options()))
+  return(rp$get_values())
 }
 
 record_data <- R6::R6Class(

R/record-estimation.R
@@ -7,7 +7,7 @@ parse_estimation_record <- function() {
   rp$process_options()
   rp$elems_assert_done()
 
-  return(list(template = rp$get_template(), options = rp$get_options()))
+  return(rp$get_values())
 }
 
 record_estimation <- R6::R6Class(

commit ca21b40201f273554d98768d29d48768f2372782

restore formatting of whitespace before record name
Originally record_parser put a special "record_name" value in the
template.  This value could be preceded by whitespace.  dd51347
(rework template class to use a single list, 2023-04-01) broke that by
dropping the special element and making record$format()
unconditionally append the record name _in front_ of any template
items.

Fix this regression by adding a dedicated option for the record name.
Using an option rather than a special string means there's no special
format handling needed and allows users to modify the record name in
the same way they would another option.

R/option.R
@@ -70,6 +70,16 @@ option_value <- R6::R6Class(
   )
 )
 
+option_record_name <- R6::R6Class(
+  "option_record_name",
+  inherit = option_flag,
+  public = list(
+    format = function() {
+      paste0("$", self$name_raw)
+    }
+  )
+)
+
 option_param <- R6::R6Class(
   "nmrec_option_param",
   inherit = option_pos,

R/process-options.R
@@ -31,7 +31,10 @@ process_options <- function(rp,
     if (is.null(opt)) {
       if (fail_on_unknown) {
         abort(
-          sprintf("Unknown option for $%s: %s", rp$name_raw, opt_raw),
+          c(
+            paste("Unknown option:", deparse_string(as.character(opt_raw))),
+            format(rp)
+          ),
           c("nmrec_unknown_option", "nmrec_parse_error")
         )
       }

R/record-data.R
@@ -1,7 +1,7 @@
 # TODO: Document parse_* functions somewhere.
 
 parse_data_record <- function() {
-  rp <- record_parser$new(private$name_raw, private$lines)
+  rp <- record_parser$new("data", private$name_raw, private$lines)
 
   prev <- private$previous_rec
   filename <- NULL
@@ -32,7 +32,7 @@ parse_data_record <- function() {
   rp$gobble()
 
   file_only <- !is.null(filename) ||
-    identical(purrr::map_chr(prev$get_options(), "name"), "filename")
+    identical(purrr::pluck(prev$get_options(), -1, "name"), "filename")
   # (format)
   if (file_only && rp$is("paren_open")) {
     end <- rp$find_closing_paren()

R/record-estimation.R
@@ -1,5 +1,5 @@
 parse_estimation_record <- function() {
-  rp <- record_parser$new(private$name_raw, private$lines)
+  rp <- record_parser$new("estimation", private$name_raw, private$lines)
   process_options(rp, estimation_option_types, estimation_option_names)
   rp$assert_done()
 

R/record-omega.R
@@ -1,5 +1,5 @@
 parse_omega_record <- function() {
-  rp <- record_parser$new(private$name_raw, private$lines)
+  rp <- record_parser$new("omega", private$name_raw, private$lines)
 
   prev <- private$previous_rec
   if (!is.null(prev)) {

R/record-parser.R
@@ -3,8 +3,6 @@
 #' Fields
 #' ------
 #'
-#'  * name_raw: the name of the record as specified in the control stream.
-#'
 #'  * elems: list of `nmrec_element` objects and plain strings derived by
 #'    passing the `lines` argument to `split_to_elements()`.
 #'
@@ -86,30 +84,29 @@
 record_parser <- R6::R6Class(
   "nmrec_record_parser",
   public = list(
-    name_raw = NULL,
     elems = NULL,
     n_elems = NULL,
     idx_e = 1L,
     lstr = NULL,
-    initialize = function(name_raw, lines) {
-      self$name_raw <- name_raw
-
+    initialize = function(name, name_raw, lines) {
       self$elems <- split_to_elements(lines)
       self$n_elems <- length(self$elems)
       self$lstr <- lstring$new(self$n_elems)
 
       self$gobble_one("whitespace")
 
+      rn_opt <- option_record_name$new(name, name_raw)
       rn <- self$yank()
-      if (!identical(rn, paste0("$", name_raw))) {
+      if (!identical(rn, format(rn_opt))) {
         abort(
           c(
-            paste("First element must be", name_raw),
+            paste("First element must be", rn_opt),
             paste("got:", rn)
           ),
           "nmrec_dev_error"
         )
       }
+      self$append(rn_opt)
       self$gobble_one("whitespace")
     },
     format = function() {
@@ -127,13 +124,7 @@ record_parser <- R6::R6Class(
     },
     assert_done = function() {
       if (!self$done()) {
-        abort(
-          c(
-            sprintf("Failed to parse %s record.", self$name_raw),
-            self$format()
-          ),
-          "nmrec_parse_error"
-        )
+        abort(c("Failed to parse record.", self$format()), "nmrec_parse_error")
       }
     },
     assert_remaining = function() {

R/record-prior.R
@@ -1,5 +1,5 @@
 parse_prior_record <- function() {
-  rp <- record_parser$new(private$name_raw, private$lines)
+  rp <- record_parser$new("prior", private$name_raw, private$lines)
 
   prev <- private$previous_rec
   if (!is.null(prev)) {

R/record-sigma.R
@@ -1,5 +1,5 @@
 parse_sigma_record <- function() {
-  rp <- record_parser$new(private$name_raw, private$lines)
+  rp <- record_parser$new("sigma", private$name_raw, private$lines)
 
   prev <- private$previous_rec
   if (!is.null(prev)) {

R/record-table.R
@@ -1,5 +1,5 @@
 parse_table_record <- function() {
-  rp <- record_parser$new(private$name_raw, private$lines)
+  rp <- record_parser$new("table", private$name_raw, private$lines)
 
   is_table_list_element <- function(x) {
     lc <- tolower(x)

R/record-theta.R
@@ -1,5 +1,5 @@
 parse_theta_record <- function() {
-  rp <- record_parser$new(private$name_raw, private$lines)
+  rp <- record_parser$new("theta", private$name_raw, private$lines)
 
   prev <- private$previous_rec
   if (!is.null(prev)) {

R/record.R
@@ -53,7 +53,7 @@ record <- R6::R6Class(
       if (is.null(self$values)) {
         paste0(paste(private$lines, collapse = "\n"), "\n")
       } else {
-        paste0("$", private$name_raw, lstr_format(self$values))
+        lstr_format(self$values)
       }
     },
     parse = function() {

tests/testthat/test-read-ctl.R
@@ -124,7 +124,7 @@ test_that("parse_ctl() works: bayes1", {
   rec$parse()
   expect_identical(
     purrr::map_chr(rec$get_options(), "name"),
-    c("filename", "ignore")
+    c("data", "filename", "ignore")
   )
   expect_identical(rec$format(), "$DATA example1.csv IGNORE=C\n")
 })

tests/testthat/test-record-data.R
@@ -29,6 +29,7 @@ test_that("parse_data_record() works", {
       input = "$data foo.csv",
       want = list(
         values = list(
+          option_record_name$new("data", "data"),
           elem_whitespace(" "),
           option_pos$new("filename", value = "foo.csv"),
           elem_linebreak()
@@ -39,6 +40,7 @@ test_that("parse_data_record() works", {
       input = "$data 'foo bar.csv'",
       want = list(
         values = list(
+          option_record_name$new("data", "data"),
           elem_whitespace(" "),
           option_pos$new("filename", value = "'foo bar.csv'"),
           elem_linebreak()
@@ -49,6 +51,7 @@ test_that("parse_data_record() works", {
       input = '$data "foo bar.csv"',
       want = list(
         values = list(
+          option_record_name$new("data", "data"),
           elem_whitespace(" "),
           option_pos$new("filename", value = '"foo bar.csv"'),
           elem_linebreak()
@@ -59,6 +62,7 @@ test_that("parse_data_record() works", {
       input = c("$data foo.csv &  ", "(FE) rec 3"),
       want = list(
         values = list(
+          option_record_name$new("data", "data"),
           elem_whitespace(" "),
           option_pos$new("filename", value = "foo.csv"),
           elem_whitespace(" "), elem_ampersand(),
@@ -78,6 +82,7 @@ test_that("parse_data_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("data", "data"),
           elem_whitespace("    "),
           option_pos$new("filename", value = "foo.csv"),
           elem_whitespace(" "),
@@ -102,6 +107,7 @@ test_that("parse_data_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("data", "data"),
           elem_whitespace(" "),
           option_pos$new("filename", value = "foo.csv"),
           elem_linebreak(), elem_comment("; comment"), elem_linebreak(),
@@ -127,6 +133,7 @@ test_that("parse_data_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("data", "data"),
           elem_whitespace(" "),
           option_pos$new("filename", value = "foo.csv"),
           elem_whitespace(" "),
@@ -177,6 +184,7 @@ test_that("data records are combined", {
   expect_identical(
     recs[[2]]$values,
     list(
+      option_record_name$new("data", "data"),
       elem_whitespace(" "),
       option_pos$new("filename", value = "foo.csv"),
       elem_linebreak()
@@ -186,6 +194,7 @@ test_that("data records are combined", {
   expect_identical(
     recs[[4]]$values,
     list(
+      option_record_name$new("data", "data"),
       elem_whitespace("\t"),
       option_pos$new("format", value = "(F )"),
       elem_linebreak()
@@ -195,6 +204,7 @@ test_that("data records are combined", {
   expect_identical(
     recs[[5]]$values,
     list(
+      option_record_name$new("data", "DAT"),
       elem_whitespace(" "),
       option_value$new("ignore", name_raw = "ign", value = "C", sep = " "),
       elem_linebreak()

tests/testthat/test-record-estimation.R
@@ -18,6 +18,7 @@ test_that("parse_estimation_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("estimation", "est"),
           elem_whitespace(" "),
           option_value$new(
             "maxevals",

tests/testthat/test-record-omega.R
@@ -4,6 +4,7 @@ test_that("parse_omega_record() works", {
       input = "$OMEGA 1",
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_param$new(
             "omega",
@@ -17,6 +18,7 @@ test_that("parse_omega_record() works", {
       input = "$OMEGA diag(1) 1",
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_value$new(
             "diagonal", "diag", "(1)",
@@ -38,6 +40,7 @@ test_that("parse_omega_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_param$new(
             "omega",
@@ -63,6 +66,7 @@ test_that("parse_omega_record() works", {
       input = "$OMEGA 1,(fix 2) (3 uni,SD)",
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_param$new(
             "omega",
@@ -100,6 +104,7 @@ test_that("parse_omega_record() works", {
       input = "$OMEGA 1 (2 3) X 4",
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_param$new(
             "omega",
@@ -125,6 +130,7 @@ test_that("parse_omega_record() works", {
       input = "$OMEGA bloc(2) SAME",
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_value$new(
             "block", "bloc", "(2)",
@@ -140,6 +146,7 @@ test_that("parse_omega_record() works", {
       input = "$OMEGA BLOCK(2) same(3)",
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_value$new("block", "BLOCK", "(2)", sep = ""),
           elem_whitespace(" "),
@@ -152,6 +159,7 @@ test_that("parse_omega_record() works", {
       input = "$OMEGA BLOCK (2) FIX VAL (0.1,0.01)",
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_value$new(
             "block", "BLOCK", "(2)",
@@ -173,6 +181,7 @@ test_that("parse_omega_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_flag$new("standard", "sd"),
           elem_whitespace(" "),
@@ -206,6 +215,7 @@ test_that("parse_omega_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_linebreak(),
           option_pos$new("label", value = "foo="),
           option_param$new(
@@ -239,6 +249,7 @@ test_that("parse_omega_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_value$new("block", "BLOCK", "(4)", sep = ""),
           elem_linebreak(),
@@ -313,6 +324,7 @@ test_that("parse_omega_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("omega", "OMEGA"),
           elem_whitespace(" "),
           option_value$new("block", "BLOCK", "(4)", sep = ""),
           elem_linebreak(),
@@ -404,6 +416,7 @@ test_that("omega records are combined", {
   expect_identical(
     recs[[2]]$values,
     list(
+      option_record_name$new("omega", "omega"),
       elem_whitespace(" "),
       option_param$new(
         "omega",
@@ -416,6 +429,7 @@ test_that("omega records are combined", {
   expect_identical(
     recs[[4]]$values,
     list(
+      option_record_name$new("omega", "omega"),
       elem_whitespace(" "),
       option_value$new("block", "block", "(2)", sep = ""),
       elem_linebreak(),

tests/testthat/test-record-prior.R
@@ -17,6 +17,7 @@ test_that("parse_prior_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("prior", "PRIOR"),
           elem_whitespace(" "),
           option_flag$new("tnpri", name_raw = "TNPRI", value = TRUE),
           elem_whitespace(" "),
@@ -38,6 +39,7 @@ test_that("parse_prior_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("prior", "PRIOR"),
           elem_whitespace(" "),
           option_flag$new("tnpri", name_raw = "TNPRI", value = TRUE),
           elem_whitespace(" "),
@@ -59,6 +61,7 @@ test_that("parse_prior_record() works", {
       input = "$PRIOR nwpri ntheta=1 neta=2 nthp=3 netp=4 npexp=5",
       want = list(
         values = list(
+          option_record_name$new("prior", "PRIOR"),
           elem_whitespace(" "),
           option_flag$new("nwpri", name_raw = "nwpri", value = TRUE),
           elem_whitespace(" "),
@@ -113,6 +116,7 @@ test_that("prior records are combined", {
   expect_identical(
     recs[[2]]$values,
     list(
+      option_record_name$new("prior", "PRIOR"),
       elem_whitespace(" "),
       option_flag$new("nwpri", name_raw = "nwpri", value = TRUE),
       elem_whitespace(" "),
@@ -124,6 +128,7 @@ test_that("prior records are combined", {
   expect_identical(
     recs[[4]]$values,
     list(
+      option_record_name$new("prior", "prior"),
       elem_whitespace(" "),
       option_value$new("neta", name_raw = "neta", value = "2"),
       elem_linebreak()

tests/testthat/test-record-sigma.R
@@ -7,6 +7,7 @@ test_that("parse_sigma_record() works", {
       input = "$SIGMA 1",
       want = list(
         values = list(
+          option_record_name$new("sigma", "SIGMA"),
           elem_whitespace(" "),
           option_param$new(
             "sigma",
@@ -39,7 +40,7 @@ test_that("sigma records are combined", {
     "$prob",
     "$sigma 1",
     "$data foo.csv",
-    "$sigma 2"
+    "  $sigma 2"
   )
 
   res <- parse_ctl(lines)
@@ -54,6 +55,7 @@ test_that("sigma records are combined", {
   expect_identical(
     recs[[2]]$values,
     list(
+      option_record_name$new("sigma", "sigma"),
       elem_whitespace(" "),
       option_param$new(
         "sigma",
@@ -66,6 +68,8 @@ test_that("sigma records are combined", {
   expect_identical(
     recs[[4]]$values,
     list(
+      elem_whitespace("  "),
+      option_record_name$new("sigma", "sigma"),
       elem_whitespace(" "),
       option_param$new(
         "sigma",

tests/testthat/test-record-table.R
@@ -24,6 +24,7 @@ test_that("parse_table_record() works", {
       input = "$table ID NUM",
       want = list(
         values = list(
+          option_record_name$new("table", "table"),
           elem_whitespace(" "),
           option_pos$new("list1", value = "ID NUM"),
           elem_linebreak()
@@ -34,6 +35,7 @@ test_that("parse_table_record() works", {
       input = c("$table ID NUM ; comment", " PRED  noa,rformat=1"),
       want = list(
         values = list(
+          option_record_name$new("table", "table"),
           elem_whitespace(" "),
           option_pos$new("list1", value = "ID NUM ; comment\n PRED"),
           elem_whitespace("  "),
@@ -48,6 +50,7 @@ test_that("parse_table_record() works", {
       input = "$table a EXCLUDE_BY b lasto by c d nop by e",
       want = list(
         values = list(
+          option_record_name$new("table", "table"),
           elem_whitespace(" "),
           option_pos$new("list1", value = "a"),
           elem_whitespace(" "),
@@ -68,6 +71,7 @@ test_that("parse_table_record() works", {
       input = "$table id wres",
       want = list(
         values = list(
+          option_record_name$new("table", "table"),
           elem_whitespace(" "),
           option_pos$new("list1", value = "id wres"),
           elem_linebreak()
@@ -78,6 +82,7 @@ test_that("parse_table_record() works", {
       input = "$table wres",
       want = list(
         values = list(
+          option_record_name$new("table", "table"),
           elem_whitespace(" "),
           option_flag$new("wreschol", name_raw = "wres", value = TRUE),
           elem_linebreak()
@@ -88,6 +93,7 @@ test_that("parse_table_record() works", {
       input = "$table id noapp wres",
       want = list(
         values = list(
+          option_record_name$new("table", "table"),
           elem_whitespace(" "),
           option_pos$new("list1", value = "id"),
           elem_whitespace(" "),
@@ -102,6 +108,7 @@ test_that("parse_table_record() works", {
       input = "$table id noapp file='quo;ted'",
       want = list(
         values = list(
+          option_record_name$new("table", "table"),
           elem_whitespace(" "),
           option_pos$new("list1", value = "id"),
           elem_whitespace(" "),

tests/testthat/test-record-theta.R
@@ -14,6 +14,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA 1",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -27,6 +28,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA  (1,2,3)",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace("  "),
           option_param$new(
             "theta",
@@ -48,6 +50,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA  (1,2  3)",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace("  "),
           option_param$new(
             "theta",
@@ -69,6 +72,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA 1 fix",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -86,6 +90,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA 1 uni",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -103,6 +108,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA 1 unint, fixed",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -125,6 +131,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA  (1,,3)",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace("  "),
           option_param$new(
             "theta",
@@ -144,6 +151,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA (FIX 2)",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -163,6 +171,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA  (1,2,3 fixed)",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace("  "),
           option_param$new(
             "theta",
@@ -189,6 +198,7 @@ test_that("parse_theta_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -213,6 +223,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA (1,2,3) fixed numberpts=3",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -245,6 +256,7 @@ test_that("parse_theta_record() works", {
       ),
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -287,6 +299,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA foo=1",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_pos$new("label", value = "foo="),
           option_param$new(
@@ -301,6 +314,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA foo= 1",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_pos$new("label", value = "foo="),
           elem_whitespace(" "),
@@ -316,6 +330,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA foo=(1,2)",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_pos$new("label", value = "foo="),
           option_param$new(
@@ -336,6 +351,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA NAMES(V1,CL,Q,V2) (0.0,5.0) (0.0,6.0) (0.0,7.0) 8",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_value$new(
             "names",
@@ -387,6 +403,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA (1,2,3)x3",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -409,6 +426,7 @@ test_that("parse_theta_record() works", {
       input = "$THETA (1,2,3) X 3",
       want = list(
         values = list(
+          option_record_name$new("theta", "THETA"),
           elem_whitespace(" "),
           option_param$new(
             "theta",
@@ -465,6 +483,7 @@ test_that("theta records are combined", {
   expect_identical(
     recs[[2]]$values,
     list(
+      option_record_name$new("theta", "theta"),
       elem_whitespace(" "),
       option_param$new(
         "theta",
@@ -477,6 +496,7 @@ test_that("theta records are combined", {
   expect_identical(
     recs[[4]]$values,
     list(
+      option_record_name$new("theta", "theta"),
       elem_whitespace(" "),
       option_param$new(
         "theta",

tests/testthat/test-write-ctl.R
@@ -24,3 +24,16 @@ test_that("format.nmrec_ctl_records() works", {
     expect_identical(readLines(tfile), ex)
   }
 })
+
+test_that("format() method preserves whitespace before records", {
+  lines <- c(
+    "$prob p",
+    " \t  $data a.csv"
+  )
+  ctl <- parse_ctl(lines)
+
+  expected <- paste0(paste(lines, collapse = "\n"), "\n")
+  expect_identical(format(ctl), expected)
+  ctl$records[[2]]$parse()
+  expect_identical(format(ctl), expected)
+})