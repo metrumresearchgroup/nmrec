09008a9a541005edf1a80eaefbb3f9068ea3af4d
Kyle Meyer <kylem@metrumrg.com>
2023-04-17 13:04:48 -0400

    silence spurious 'R CMD check' note
    'R CMD check' says that R6 isn't used despite various calls to
    R6::R6Class:
    
      Namespace in Imports field not imported from: ‘R6’
        All declared Imports should be used.
    
    This is a known issue and has now been fixed in R devel [1].  Work
    around it by following the approach suggested at
    https://github.com/hadley/r-pkgs issue 828.
    
    Note that devtools::check() does _not_ show this warning under R 4.1
    and below because it passes --as-cran by default and that
    unconditionally sets _R_CHECK_PACKAGES_USED_IGNORE_UNUSED_IMPORTS_ to
    FALSE until R 4.2 [2].
    
    [1] https://bugs.r-project.org/show_bug.cgi?id=18465
        svn rev 83774
    
    [2] https://bugs.r-project.org/show_bug.cgi?id=18229
        svn rev 81155

--- R/nmrec.R
+++ R/nmrec.R
@@ -5,3 +5,9 @@
 if (getRversion() >= "2.15.1") {
   utils::globalVariables(c("self", "private", "super"))
 }
+
+# Kludge: Until R 4.3, 'R CMD check' incorrectly flags a package as "not
+# imported from" if it's used only outside of functions.
+ignore_unused_imports <- function() {
+  R6::R6Class
+}
