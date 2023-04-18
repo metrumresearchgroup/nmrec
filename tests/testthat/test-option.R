783cf36d3ea7a3345b98bafb3856ab1f609e4ab6
Kyle Meyer <kylem@metrumrg.com>
2023-04-18 09:01:12 -0400

    option: test formatting with NULL values

--- tests/testthat/test-option.R
+++ tests/testthat/test-option.R
@@ -3,3 +3,28 @@ test_that("option$name is read-only", {
   expect_identical(opt$name, "foo")
   expect_error(opt$name <- "x", class = "nmrec_error")
 })
+
+test_that("options with NULL values format to empty strings", {
+  cases <- list(
+    list(
+      opt = option_pos,
+      args = list("foo", value = NULL)
+    ),
+    list(
+      opt = option_flag,
+      args = list("foo", "foo", value = NULL)
+    ),
+    list(
+      opt = option_value,
+      args = list("foo", "foo", value = NULL)
+    ),
+    list(
+      opt = option_nested,
+      args = list("foo", values = NULL)
+    )
+  )
+
+  for (case in cases) {
+    expect_identical(format(do.call(case$opt$new, args = case$args)), "")
+  }
+})
