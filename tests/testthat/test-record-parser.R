8246c0ede04c14d74ffe6676d981ab15e6e56293
Kyle Meyer <kylem@metrumrg.com>
2023-04-18 09:09:20 -0400

    record_parser: test find_next boundary behavior

--- tests/testthat/test-record-parser.R
+++ tests/testthat/test-record-parser.R
@@ -4,3 +4,9 @@ test_that("record_parser errors on name_raw/lines mismatch", {
     class = "nmrec_dev_error"
   )
 })
+
+test_that("record_parser$find_next() returns 0 if no elems remain", {
+  rp <- record_parser$new("foo", "foo", "$foo")
+  rp$gobble()
+  expect_identical(rp$find_next(), 0L)
+})
