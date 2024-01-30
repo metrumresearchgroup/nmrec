7d6f245f7bd599d185fbfe9b9fea017f797dd70a
Kyle Meyer <kylem@metrumrg.com>
2024-01-30 18:17:01 -0500

    extract-param tests: simplify definition of expected attributes
    Use structure() to define the nmrec_flags attribute alongside the main
    value for 'want' rather than having special handling for a separate
    'want_attr' value.

--- tests/testthat/test-extract-param.R
+++ tests/testthat/test-extract-param.R
@@ -41,9 +41,11 @@ test_that("extract_theta(..., mark_flags = ...) works", {
     list(
       lines = "$theta 1 2 3",
       flags = "fix",
-      want = c(1, 2, 3),
-      want_attr = list(
-        fixed = c(FALSE, FALSE, FALSE)
+      want = structure(
+        c(1, 2, 3),
+        nmrec_flags = list(
+          fixed = c(FALSE, FALSE, FALSE)
+        )
       )
     ),
     list(
@@ -53,44 +55,50 @@ test_that("extract_theta(..., mark_flags = ...) works", {
         "$theta 4 5"
       ),
       flags = "fix",
-      want = c(1, 2, 3, 4, 5),
-      want_attr = list(
-        fixed = c(FALSE, FALSE, TRUE, FALSE, FALSE)
+      want = structure(
+        c(1, 2, 3, 4, 5),
+        nmrec_flags = list(
+          fixed = c(FALSE, FALSE, TRUE, FALSE, FALSE)
+        )
       )
     ),
     list(
       lines = "$theta (1.0 1.1 1.2 fix) 2 (fix 3.0, 3.1)",
       flags = "fix",
-      want = c(1.1, 2, 3.1),
-      want_attr = list(
-        fixed = c(TRUE, FALSE, TRUE)
+      want = structure(
+        c(1.1, 2, 3.1),
+        nmrec_flags = list(
+          fixed = c(TRUE, FALSE, TRUE)
+        )
       )
     ),
     list(
       lines = "$theta 1 FIX (2.0,,2.1 fix) 3 UNINT",
       flags = "fix",
-      want = c(1, NA_real_, 3),
-      want_attr = list(
-        fixed = c(TRUE, TRUE, FALSE)
+      want = structure(
+        c(1, NA_real_, 3),
+        nmrec_flags = list(
+          fixed = c(TRUE, TRUE, FALSE)
+        )
       )
     ),
     list(
       lines = "$theta (1.0,1.1,1.2 fix)x3 4 UNINT",
       flags = c("fix", "uni"),
-      want = c(1.1, NA_real_, NA_real_, 4),
-      want_attr = list(
-        fixed = c(TRUE, NA, NA, FALSE),
-        unint = c(FALSE, NA, NA, TRUE)
+      want = structure(
+        c(1.1, NA_real_, NA_real_, 4),
+        nmrec_flags = list(
+          fixed = c(TRUE, NA, NA, FALSE),
+          unint = c(FALSE, NA, NA, TRUE)
+        )
       )
     )
   )
   for (case in cases) {
     ctl <- parse_ctl(c(prob_line, case$lines))
-    want <- case$want
-    attr(want, "nmrec_flags") <- case$want_attr
     expect_identical(
       extract_theta(ctl, mark_flags = case$flags),
-      want
+      case$want
     )
   }
 })
@@ -265,20 +273,22 @@ test_that("extract_omega(..., mark_flags = ...) works", {
     list(
       lines = "$omega 1 2 fix",
       flags = "fix",
-      want = matrix(
-        c(
-          1, NA_real_,
-          NA_real_, 2
-        ),
-        nrow = 2, ncol = 2, byrow = TRUE
-      ),
-      want_attr = list(
-        fixed = matrix(
+      want = structure(
+        matrix(
           c(
-            FALSE, NA,
-            NA, TRUE
+            1, NA_real_,
+            NA_real_, 2
           ),
           nrow = 2, ncol = 2, byrow = TRUE
+        ),
+        nmrec_flags = list(
+          fixed = matrix(
+            c(
+              FALSE, NA,
+              NA, TRUE
+            ),
+            nrow = 2, ncol = 2, byrow = TRUE
+          )
         )
       )
     ),
@@ -289,20 +299,22 @@ test_that("extract_omega(..., mark_flags = ...) works", {
         "0.1 4"
       ),
       flags = "fix",
-      want = matrix(
-        c(
-          3, NA_real_,
-          0.1, 4
-        ),
-        nrow = 2, ncol = 2, byrow = TRUE
-      ),
-      want_attr = list(
-        fixed = matrix(
+      want = structure(
+        matrix(
           c(
-            TRUE, NA,
-            TRUE, TRUE
+            3, NA_real_,
+            0.1, 4
           ),
           nrow = 2, ncol = 2, byrow = TRUE
+        ),
+        nmrec_flags = list(
+          fixed = matrix(
+            c(
+              TRUE, NA,
+              TRUE, TRUE
+            ),
+            nrow = 2, ncol = 2, byrow = TRUE
+          )
         )
       )
     ),
@@ -313,27 +325,29 @@ test_that("extract_omega(..., mark_flags = ...) works", {
         "0.1 uni 4"
       ),
       flags = c("fix", "unint"),
-      want = matrix(
-        c(
-          3, NA_real_,
-          0.1, 4
-        ),
-        nrow = 2, ncol = 2, byrow = TRUE
-      ),
-      want_attr = list(
-        fixed = matrix(
+      want = structure(
+        matrix(
           c(
-            TRUE, NA,
-            TRUE, TRUE
+            3, NA_real_,
+            0.1, 4
           ),
           nrow = 2, ncol = 2, byrow = TRUE
         ),
-        unint = matrix(
-          c(
-            TRUE, NA,
-            TRUE, TRUE
+        nmrec_flags = list(
+          fixed = matrix(
+            c(
+              TRUE, NA,
+              TRUE, TRUE
+            ),
+            nrow = 2, ncol = 2, byrow = TRUE
           ),
-          nrow = 2, ncol = 2, byrow = TRUE
+          unint = matrix(
+            c(
+              TRUE, NA,
+              TRUE, TRUE
+            ),
+            nrow = 2, ncol = 2, byrow = TRUE
+          )
         )
       )
     ),
@@ -346,64 +360,68 @@ test_that("extract_omega(..., mark_flags = ...) works", {
         "0.1 cor 4"
       ),
       flags = c("SD", "fix", "corre"),
-      want = matrix(
-        c(
-          1, NA_real_, NA_real_, NA_real_,
-          NA_real_, 2, NA_real_, NA_real_,
-          NA_real_, NA_real_, 3, NA_real_,
-          NA_real_, NA_real_, 0.1, 4
-        ),
-        nrow = 4, ncol = 4, byrow = TRUE
-      ),
-      want_attr = list(
-        standard = matrix(
+      want = structure(
+        matrix(
           c(
-            TRUE, NA, NA, NA,
-            NA, FALSE, NA, NA,
-            NA, NA, FALSE, NA,
-            NA, NA, FALSE, FALSE
+            1, NA_real_, NA_real_, NA_real_,
+            NA_real_, 2, NA_real_, NA_real_,
+            NA_real_, NA_real_, 3, NA_real_,
+            NA_real_, NA_real_, 0.1, 4
           ),
           nrow = 4, ncol = 4, byrow = TRUE
         ),
-        fixed = matrix(
-          c(
-            TRUE, NA, NA, NA,
-            NA, FALSE, NA, NA,
-            NA, NA, TRUE, NA,
-            NA, NA, TRUE, TRUE
+        nmrec_flags = list(
+          standard = matrix(
+            c(
+              TRUE, NA, NA, NA,
+              NA, FALSE, NA, NA,
+              NA, NA, FALSE, NA,
+              NA, NA, FALSE, FALSE
+            ),
+            nrow = 4, ncol = 4, byrow = TRUE
           ),
-          nrow = 4, ncol = 4, byrow = TRUE
-        ),
-        correlation = matrix(
-          c(
-            FALSE, NA, NA, NA,
-            NA, FALSE, NA, NA,
-            NA, NA, TRUE, NA,
-            NA, NA, TRUE, TRUE
+          fixed = matrix(
+            c(
+              TRUE, NA, NA, NA,
+              NA, FALSE, NA, NA,
+              NA, NA, TRUE, NA,
+              NA, NA, TRUE, TRUE
+            ),
+            nrow = 4, ncol = 4, byrow = TRUE
           ),
-          nrow = 4, ncol = 4, byrow = TRUE
+          correlation = matrix(
+            c(
+              FALSE, NA, NA, NA,
+              NA, FALSE, NA, NA,
+              NA, NA, TRUE, NA,
+              NA, NA, TRUE, TRUE
+            ),
+            nrow = 4, ncol = 4, byrow = TRUE
+          )
         )
       )
     ),
     list(
       lines = "$omega block(3) FIX VAL(1, 0.1)",
       flags = "fix",
-      want = matrix(
-        c(
-          1, NA_real_, NA_real_,
-          0.1, NA_real_, NA_real_,
-          NA_real_, NA_real_, NA_real_
-        ),
-        nrow = 3, ncol = 3, byrow = TRUE
-      ),
-      want_attr = list(
-        fixed = matrix(
+      want = structure(
+        matrix(
           c(
-            TRUE, NA, NA,
-            TRUE, NA, NA,
-            NA, NA, NA
+            1, NA_real_, NA_real_,
+            0.1, NA_real_, NA_real_,
+            NA_real_, NA_real_, NA_real_
           ),
           nrow = 3, ncol = 3, byrow = TRUE
+        ),
+        nmrec_flags = list(
+          fixed = matrix(
+            c(
+              TRUE, NA, NA,
+              TRUE, NA, NA,
+              NA, NA, NA
+            ),
+            nrow = 3, ncol = 3, byrow = TRUE
+          )
         )
       )
     )
@@ -411,10 +429,9 @@ test_that("extract_omega(..., mark_flags = ...) works", {
   for (case in cases) {
     ctl <- parse_ctl(c(prob_line, case$lines))
     want <- case$want
-    attr(want, "nmrec_flags") <- case$want_attr
     expect_identical(
       extract_omega(ctl, mark_flags = case$flags),
-      want
+      case$want
     )
   }
 })
@@ -423,27 +440,29 @@ test_that("extract_sigma() works", {
   # Note: The extract_omega() tests above covers most the shared matrix logic.
   ctl <- parse_ctl(c(prob_line, "$sigma block(2) FIX SD 1 2 3"))
   got <- extract_sigma(ctl, mark_flags = c("fixed", "SD"))
-  want <- matrix(
-    c(
-      1, NA_real_,
-      2, 3
-    ),
-    nrow = 2, ncol = 2, byrow = TRUE
-  )
-  attr(want, "nmrec_flags") <- list(
-    fixed = matrix(
+  want <- structure(
+    matrix(
       c(
-        TRUE, NA,
-        TRUE, TRUE
+        1, NA_real_,
+        2, 3
       ),
       nrow = 2, ncol = 2, byrow = TRUE
     ),
-    standard = matrix(
-      c(
-        TRUE, NA,
-        TRUE, TRUE
+    nmrec_flags = list(
+      fixed = matrix(
+        c(
+          TRUE, NA,
+          TRUE, TRUE
+        ),
+        nrow = 2, ncol = 2, byrow = TRUE
       ),
-      nrow = 2, ncol = 2, byrow = TRUE
+      standard = matrix(
+        c(
+          TRUE, NA,
+          TRUE, TRUE
+        ),
+        nrow = 2, ncol = 2, byrow = TRUE
+      )
     )
   )
   expect_identical(got, want)
